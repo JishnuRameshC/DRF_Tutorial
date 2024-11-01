432547e183da392509cec93332658899c7865f6f
JishnuRameshC <jishnurameshc@gmail.com>
2024-11-01 09:56:06 +0530

    Deleted unnecessary files

--- apis/views.py
+++
@@ -1,9 +0,0 @@
-from books.models import Book
-from rest_framework import generics
-from .serializer import BookSerializer
-# Create your views here.
-
-
-class BookAPIListView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
--- todos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.TodoListView.as_view(), name='todo_list'),
-    path('<int:pk>/', views.TodoDetailView.as_view(), name='todo_detail'),
-
-]
